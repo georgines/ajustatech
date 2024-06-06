001d857dff1306614fcf0f6e08ed0bf4a24df7cd
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2024-06-06 16:11:38 +0000

    Revert "criando modulo core e o modulo de servicos de impressao"
    This reverts commit 2dd9bcb9242ab918a0b3e6caa0c89e3d201a758a.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-    App\Providers\AliaseServiceProvider::class,
     App\Providers\AppServiceProvider::class,
+    App\Providers\AliaseServiceProvider::class,
     App\Providers\MenuServiceProvider::class,
-    Ajustatech\Core\Providers\CoreServiceProvider::class
 ];
--- modules/Ajustatech/PrintService/src/views/cupom.blade.php
+++
@@ -1,7 +0,0 @@
-<html>
-    <body>
-        <h1 class="mb-10">Gerador de cupom</h1>
-        {!! $html !!}
-    </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</html>
