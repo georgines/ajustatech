db75c58bfcb33c8dfb65192cce9da81793ea07f4
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2024-08-04 19:19:05 +0000

    removendo classe nao usada

--- modules/Ajustatech/Core/src/Providers/CoreServiceProvider.php
+++ modules/Ajustatech/Core/src/Providers/CoreServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Ajustatech\Core\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Ajustatech\NewModule\Providers\NewModuleServiceProvider;
 
 class CoreServiceProvider extends ServiceProvider
 {
@@ -11,8 +10,7 @@ class CoreServiceProvider extends ServiceProvider
     public function register(): void
     { 
         $this->app->register(CommandServiceProvider::class);
-        $this->app->register(MenuServiceProvider::class);
-        $this->app->register(NewModuleServiceProvider::class);
+        $this->app->register(MenuServiceProvider::class);       
         $this->app->register(ViewServiceProvider::class);
     }
 
