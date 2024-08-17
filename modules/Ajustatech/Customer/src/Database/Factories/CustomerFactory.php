f3c1097019a7ea98c06212154810224c02079aa6
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2024-08-17 16:39:27 +0000

    registrando a factory do customer e adicionando o modelo a factory

--- modules/Ajustatech/Customer/src/Database/Factories/CustomerFactory.php
+++ modules/Ajustatech/Customer/src/Database/Factories/CustomerFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CustomerFactory extends Factory
 {
     protected $model = Customer::class;
-    
+
 	public function definition(): array
 	{
 
--- modules/Ajustatech/Customer/src/Providers/CustomerServiceProvider.php
+++ modules/Ajustatech/Customer/src/Providers/CustomerServiceProvider.php
@@ -5,36 +5,51 @@ namespace Ajustatech\Customer\Providers;
 use Livewire\Livewire;
 use Illuminate\Support\ServiceProvider;
 use Ajustatech\Core\Helpers\MenuManagerInterface;
+use Ajustatech\Customer\Livewire\ShowCustomer;
+use Ajustatech\Customer\Livewire\CustomerManagement;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class CustomerServiceProvider extends ServiceProvider
 {
-   
+    protected $path = __DIR__ . "/..";
+
     public function register()
-    {  
-        $this->loadRoutesFrom(base_path("modules/Ajustatech/Customer/src/Routes/web.php"));
-        $this->loadViewsFrom(base_path("modules/Ajustatech/Customer/src/Views"), "customer");
-        $this->loadMigrationsFrom(base_path("modules/Ajustatech/Customer/src/migrations"));
+    {
+        $this->loadRoutesFrom("$this->path/Routes/web.php");
+        $this->loadViewsFrom("$this->path/views", "customer");
+        $this->loadMigrationsFrom("$this->path/Database/migrations");
     }
 
     public function boot()
     {
-        $this->bootRegisterMenuManager();
-        $this->bootRegisterLivewireComponentes();        
+        $this->initializeMenus();
+        $this->initializeLivewireComponents();
+        $this->initializeFactories();
     }
 
-    public function bootRegisterMenuManager()
+    public function initializeMenus()
     {
-        $verticalMenu = json_decode(file_get_contents( base_path("modules/Ajustatech/Customer/src/menu/verticalMenu.json")));
-        $horizontalMenu = json_decode(file_get_contents( base_path("modules/Ajustatech/Customer/src/menu/verticalMenu.json")));
+        $verticalMenu = json_decode(file_get_contents("$this->path/menu/verticalMenu.json"));
+        $horizontalMenu = json_decode(file_get_contents("$this->path/menu/verticalMenu.json"));
 
         $menu = app(MenuManagerInterface::class);
         $menu->addVerticalMenu($verticalMenu);
         $menu->addHorizontalMenu($horizontalMenu);
     }
 
-    public function bootRegisterLivewireComponentes()
+    public function initializeLivewireComponents()
     {
-        Livewire::component('show-customer', \Ajustatech\Customer\Livewire\ShowCustomer::class);
-        Livewire::component('customer-management', \Ajustatech\Customer\Livewire\CustomerManagement::class);
+        Livewire::component('show-customer', ShowCustomer::class);
+        Livewire::component('customer-management', CustomerManagement::class);
+    }
+
+    public function initializeFactories()
+    {
+        Factory::guessFactoryNamesUsing(function (string $modelName) {
+            $namespace = "Ajustatech\\Customer\\Database";
+            $modelName = Str::replace("$namespace\\Models\\", '', $modelName);
+            return "$namespace\\Factories\\" . $modelName . "Factory";
+        });
     }
 }
