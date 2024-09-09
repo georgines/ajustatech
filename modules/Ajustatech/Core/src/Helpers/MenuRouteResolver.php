1b714249c364f84398ad60692c569ef4f7fe1af9
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2024-09-09 01:54:59 +0000

    removendo os comentarios do MenuRouteResolver

--- modules/Ajustatech/Core/src/Helpers/MenuRouteResolver.php
+++ modules/Ajustatech/Core/src/Helpers/MenuRouteResolver.php
@@ -8,24 +8,20 @@ use stdClass;
 class MenuRouteResolver implements MenuRouteResolverInterface
 {
     public function resolveRoutes(stdClass $menu): stdClass
-    {
-        // Verifica se o JSON contém o campo 'menu' e o processa
+    {       
         if (isset($menu->menu)) {
             $menu->menu = $this->processMenu($menu->menu);
         }
-
-        // Retorna o objeto JSON com o menu processado
         return $menu;
     }
 
     protected function processMenu(array $menu): array
     {
-        $queue = $menu;  // Usamos uma fila para processar itens de menu e submenus iterativamente
+        $queue = $menu;
 
         foreach ($queue as &$menuItem) {
             $this->processMenuItem($menuItem);
-
-            // Se o item tiver submenu, adicionamos os submenus à fila para processar posteriormente
+            
             if (isset($menuItem->submenu) && is_array($menuItem->submenu)) {
                 foreach ($menuItem->submenu as &$submenuItem) {
                     $this->processMenuItem($submenuItem);
@@ -37,13 +33,11 @@ class MenuRouteResolver implements MenuRouteResolverInterface
     }
 
     protected function processMenuItem(stdClass &$menuItem): void
-    {
-        // Verifica se é um menuHeader (não precisa de alteração)
+    {        
         if (isset($menuItem->menuHeader)) {
             return;
         }
-
-        // Verifica se o item tem URL e slug para criar uma URL relativa
+        
         if (isset($menuItem->url) && isset($menuItem->slug)) {
             $this->createRelativeUrl($menuItem);
         }
@@ -52,9 +46,9 @@ class MenuRouteResolver implements MenuRouteResolverInterface
     protected function createRelativeUrl(stdClass &$menuItem): void
     {
         Route::getRoutes()->refreshNameLookups();
-                
+
         if (Route::has($menuItem->slug)) {
-            $menuItem->url = url()->route($menuItem->slug, [], false); // Passando o slug corretamente
+            $menuItem->url = url()->route($menuItem->slug, [], false);
 
         }
     }
