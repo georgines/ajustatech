f852542bdd93984ba02b913ecd0e292ae6936963
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2024-09-29 23:42:54 +0000

    criando arquivo de teste para o servico das transacoes

---
+++ modules/Ajustatech/Financial/src/Tests/Feature/CompanyCashTransactionsServiceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ajustatech\Financial\Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CompanyCashTransactionsServiceTest extends TestCase
+{
+    use RefreshDatabase;   
+}
