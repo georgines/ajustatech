623e651a165a55d177342de40f839c91ff64b93e
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2024-10-06 10:02:53 +0000

    criando o cadastro de caixa

--- modules/Ajustatech/Financial/src/Livewire/CompanyCashManagement.php
+++ modules/Ajustatech/Financial/src/Livewire/CompanyCashManagement.php
@@ -2,6 +2,8 @@
 
 namespace Ajustatech\Financial\Livewire;
 
+use Ajustatech\Financial\Services\CompanyCashService;
+use Ajustatech\Financial\Services\CompanyCashServiceInterface;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -9,10 +11,53 @@ use Livewire\Component;
 class CompanyCashManagement extends Component
 {
     public $title;
+    public $name;
+    public $initialBalance;
+    public $agency;
+    public $account;
+    public $description;
+    public $isOnline = true;
+
+    protected $companyCashService;
 
     public function mount()
     {
-        $this->title = trans('financial::messages.title');
+        $this->title = trans('financial::messages.company_cash_create_title');
+
+    }
+
+    public function updatedIsOnline($value)
+    {
+        if (!$value) {
+            // Limpar agência e conta se o caixa não for online
+            $this->agency = null;
+            $this->account = null;
+        }
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'initialBalance' => 'required|numeric|min:0',
+            'agency' => 'nullable|string|max:50',
+            'account' => 'nullable|string|max:50',
+            'description' => 'nullable|string|max:255',
+        ]);
+
+        $this->companyCashService = app(CompanyCashServiceInterface::class);
+
+        $this->companyCashService->createCash(
+            $this->name,
+            $this->initialBalance,
+            $this->isOnline ? $this->agency : null,
+            $this->isOnline ? $this->account : null,
+            $this->description,
+            $this->isOnline
+        );
+
+        // session()->flash('success', trans('financial::messages.company_cash_created_success'));
+        return redirect()->route('companycash-show');
     }
 
     public function render()
--- modules/Ajustatech/Financial/src/Views/livewire/company-cash-management.blade.php
+++ modules/Ajustatech/Financial/src/Views/livewire/company-cash-management.blade.php
@@ -1,7 +1,100 @@
-<x-slot name="page_title">
-    {{$title}}
+<x-slot name='page_title'>
+    {{ $title }}
 </x-slot>
 
-<div>
-    <!-- Livewire component company-cash-management -->
-</div>
+<section>
+    <div class="row">
+        <div class="col">
+            <div class="card mb-3">
+                <div class="card-header pt-2">
+                    <h5>{{ $title }}</h5>
+                </div>
+                <form wire:submit.prevent="save">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12 col-md-6 mb-3 validation-error">
+                                <label class="form-label" for="name">Nome do Caixa</label>
+                                <div class="input-group input-group-merge">
+                                    <span id="basic-icon-default-name" class="input-group-text @error('name') is-invalid-field @enderror">
+                                        <i class="ti ti-briefcase"></i>
+                                    </span>
+                                    <input wire:model="name" type="text" class="form-control @error('name') is-invalid-field @enderror" id="name" />
+                                </div>
+                                @error('name')
+                                    <div class="error-message">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="col-12 col-md-6 mb-3 validation-error">
+                                <label class="form-label" for="initialBalance">Saldo Inicial</label>
+                                <div class="input-group input-group-merge">
+                                    <span id="basic-icon-default-balance" class="input-group-text @error('initialBalance') is-invalid-field @enderror">
+                                        <i class="ti ti-currency-dollar"></i>
+                                    </span>
+                                    <input wire:model="initialBalance" type="text" class="form-control @error('initialBalance') is-invalid-field @enderror" id="initialBalance" />
+                                </div>
+                                @error('initialBalance')
+                                    <div class="error-message">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-check form-switch mb-3">
+                            <input wire:model="isOnline" class="form-check-input" type="checkbox" id="isOnline">
+                            <label class="form-check-label" for="isOnline">Caixa Online</label>
+                        </div>
+
+                        @if($isOnline)
+                            <div class="row">
+                                <div class="col-12 col-md-6 mb-3 validation-error">
+                                    <label class="form-label" for="agency">Agência</label>
+                                    <div class="input-group input-group-merge">
+                                        <span id="basic-icon-default-agency" class="input-group-text @error('agency') is-invalid-field @enderror">
+                                            <i class="ti ti-bank"></i>
+                                        </span>
+                                        <input wire:model="agency" type="text" class="form-control @error('agency') is-invalid-field @enderror" id="agency" />
+                                    </div>
+                                    @error('agency')
+                                        <div class="error-message">{{ $message }}</div>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12 col-md-6 mb-3 validation-error">
+                                    <label class="form-label" for="account">Conta</label>
+                                    <div class="input-group input-group-merge">
+                                        <span id="basic-icon-default-account" class="input-group-text @error('account') is-invalid-field @enderror">
+                                            <i class="ti ti-credit-card"></i>
+                                        </span>
+                                        <input wire:model="account" type="text" class="form-control @error('account') is-invalid-field @enderror" id="account" />
+                                    </div>
+                                    @error('account')
+                                        <div class="error-message">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>
+                        @endif
+
+                        <div class="row">
+                            <div class="col-12 mb-3 validation-error">
+                                <label class="form-label" for="description">Descrição</label>
+                                <div class="input-group input-group-merge">
+                                    <span id="basic-icon-default-description" class="input-group-text @error('description') is-invalid-field @enderror">
+                                        <i class="ti ti-align-left"></i>
+                                    </span>
+                                    <textarea wire:model="description" id="description" class="form-control @error('description') is-invalid-field @enderror"></textarea>
+                                </div>
+                                @error('description')
+                                    <div class="error-message">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary me-sm-3 me-1">Salvar</button>
+                        {{-- <a href="{{ route('company-cash-list') }}" class="btn btn-label-secondary">Cancelar</a> --}}
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
