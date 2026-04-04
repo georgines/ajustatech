0f4e97a57e8fd8bcf97d7d628299446cfa61c4c4
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2026-04-03 21:41:40 -0300

    configurações: atualiza .env.example para definir variáveis de ambiente e adiciona suporte ao MongoDB no arquivo database.php

--- config/database.php
+++ config/database.php
@@ -109,6 +109,12 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('MONGODB_URI', 'mongodb://127.0.0.1:27020'),
+            'database' => env('MONGODB_DATABASE', 'ajustatech'),
+        ],
+
     ],
 
     /*
