7cf77acd1108df13da6e39109f3a369cf68d1254
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2026-04-12 13:18:07 -0300

    Adiciona arquivos de dashboard-analytics, incluindo scripts e estilos, e atualiza a configuração do Vite para incluir novos bundles sob demanda

---
+++ resources/assets/js/bundles/dashboard-analytics.js
@@ -0,0 +1,5 @@
+import '../../vendor/libs/apex-charts/apexcharts.js';
+import '../../vendor/libs/swiper/swiper.js';
+import '../../vendor/libs/datatables-bs5/datatables-bootstrap5.js';
+
+import '../dashboards-analytics.js';
